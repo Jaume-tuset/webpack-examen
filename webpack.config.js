077e054e06bc7a5ce7ba9acd19d20edc1111f6b6
Jaume Tuset <tusetjaume7@gmail.com>
2023-03-02 05:48:41 +0100

    webpack inicial

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // o production
+ entry: './src/index.js',
+ output: {
+   filename: 'main.js',
+   path: path.resolve(__dirname, 'dist'),
+ },
+ devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  }, 
+};
